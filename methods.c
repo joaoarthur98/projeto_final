87613897f611ddb8cfb0c39220df677b47a137a6
João Arthur <joaoarthur.ufrn@gmail.com>
2019-11-13 00:08:09 -0300

    Adição de método incompleto para desenho de linhas

--- methods.c
+++ methods.c
@@ -41,19 +41,6 @@ void limparImagem(Pixel **cores, char linha[], char* m, char* n, char* r, char* 
             cores[i][j].b = atoi(b);
         }
     }
-    // Abre a imagem baseado no que foi lido na linha
-    FILE *imagem = fopen("teste.ppm", "a");
-
-    // Preenche a imagem com os valores de cores em todos os seus pixels
-    for(i = 0; i < m1; i++){
-        for(j = 0; j < n1; j++){
-            fprintf(imagem, "%u ", cores[i][j].r);
-            fprintf(imagem, "%u ", cores[i][j].g);
-            fprintf(imagem, "%u \n", cores[i][j].b);
-        }
-    }
-    // Fecha a imagem
-    fclose(imagem);
 }
 
 void renomearImagem(char linha[]){
@@ -69,27 +56,58 @@ void renomearImagem(char linha[]){
     fclose(imagem);
 }
 
-void desenharLinha(int x0, int y0, int x1, int y1){
-    int dx, dy, p, x, y;
+void desenharLinha(Pixel **cores, char linha[], char* m, char* n){
+    char comando[10];
+    char x1[5];
+    char x2[5];
+    char y1[5];
+    char y2[5];
+    int i, j;
 
-    Polygon poligono;
+    sscanf(linha, "%s %s %s %s %s", comando, x1, y1, x2, y2);
 
-    dx = x1 - x0;
-    dy = y1 - y0;
+    int x1_int = atoi(x1);
+    int x2_int = atoi(x2);
+    int y1_int = atoi(y1);
+    int y2_int = atoi(y2);
 
-    x = x0;
-    y = y0;
+    int x = x1_int;
+    int y = y1_int;
 
-    p = 2*dy-dx;
+    // Inclinação
+    int dx = abs(x2_int-x1_int);
+    int dy = abs(y2_int-y1_int);
 
-    while(x < x1){
-        if(p>=0){
-            
+    int decisao = (2*dy-dx);
+    int incI = 2*dy;
+    int incS = 2*(dy-dx);
+
+    cores[y][x].r = 0;
+    cores[y][x].g = 0;
+    cores[y][x].b = 0;
+
+    while(x != x2_int){
+        if(decisao >= 0){
+            decisao = decisao+2*dy-2*dx;
+            //x++;
+            //y++;
         }
+        else{
+            decisao = decisao + 2*dy;
+            //y++;
+        }
+        x++;
+    cores[y][x].r = 0;
+    cores[y][x].g = 0;
+    cores[y][x].b = 0;
     }
 }
+
+
 // Função para alocar dinamicamente cores
-Pixel** alocar_pixels(char* m, char* n){
+Pixel** alocar_pixels(char linha[], char* m, char* n){
+    char comando[10];
+    sscanf(linha, "%s %s %s", comando, m, n);
     int m1 = atoi(m);
     int n1 = atoi(n);
     int i;
@@ -99,4 +117,59 @@ Pixel** alocar_pixels(char* m, char* n){
         cores[i] = (Pixel*)malloc(n1 * sizeof(Pixel));
     }
     return cores;
+}
+
+void pintarImagem(Pixel** cores, char linha[], char* m, char* n, char* r, char* g, char* b){
+     int m1 = atoi(m);
+    int n1 = atoi(n);
+    int i, j;
+    char comando[10];
+    // Lê a linha no padrão especificado no parâmetro do meio
+    sscanf(linha, "%s %s %s %s", comando, r, g, b);
+    
+    // Percorre cores, preenchendo cada pixel com as cores especificadas pela linha lida
+    for(i = 0; i < m1; i++){
+        for(j = 0; j < n1; j++){
+            cores[i][j].r = atoi(r);
+            cores[i][j].g = atoi(g);
+            cores[i][j].b = atoi(b);
+        }
+    }
+    // Abre a imagem baseado no que foi lido na linha
+    //FILE *imagem = fopen("teste.ppm", "a+");
+
+    // // Preenche a imagem com os valores de cores em todos os seus pixels
+    // for(i = 0; i < m1; i++){
+    //     for(j = 0; j < n1; j++){
+    //         fprintf(imagem, "%u ", cores[i][j].r);
+    //         fprintf(imagem, "%u ", cores[i][j].g);
+    //         fprintf(imagem, "%u \n", cores[i][j].b);
+    //     }
+    // }
+    // // Fecha a imagem
+    // fclose(imagem);
+}
+
+void criarArquivo2(char linha[], Pixel** cores, char* m, char* n){
+    char comando[10];
+    char nome[20];
+    int i, j;
+    int m1 = atoi(m);
+    int n1 = atoi(n);
+
+    sscanf(linha, "%s %s", comando, nome);
+
+    FILE *imagem = fopen(nome, "w");
+
+    fprintf(imagem, "P3\n");
+    fprintf(imagem, "%s %s\n", m, n);
+    fprintf(imagem, "255\n");
+
+    for(i = 0; i < m1; i++){
+        for(j = 0; j < n1; j++){
+            fprintf(imagem, "%u ", cores[i][j].r);
+            fprintf(imagem, "%u ", cores[i][j].g);
+            fprintf(imagem, "%u \n", cores[i][j].b); 
+        }
+    }
 }
--- methods.h
+++ methods.h
@@ -15,7 +15,10 @@ char* alocar_din(int tamanho);
 void criarArquivo(char linha[], char *m, char *n);
 void limparImagem(Pixel **cores, char linha[], char* m, char* n, char* r, char* g, char* b);
 void renomearImagem(char linha[]);
-Pixel** alocar_pixels(char* m, char* n);
+Pixel** alocar_pixels(char linha[], char* m, char* n);
+void desenharLinha(Pixel **cores, char linha[], char* m, char* n);
+void pintarImagem(Pixel** cores, char linha[], char* m, char* n, char* r, char* g, char* b);
+void criarArquivo2(char linha[], Pixel** cores, char* m, char* n);
 // void alocarImagem(FILE *arquivo, Pixel **img, int m, int n);
 // void lerArquivo(FILE *arquivo, Pixel **img,  int m, int n);
 
